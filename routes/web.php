6755b7f8e68bf9878243531e62c1c9740ab36775
Umar Raza <umarraza@users.github.com>
2021-07-19 00:08:47 +0500

    fix

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController@welcome');
 
 // Route::get('/clear', function () {
 //     Artisan::call('cache:clear');
